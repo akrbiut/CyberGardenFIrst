fc447507c43e6ec15e2dfecff56eae2d64bad1d7
IDK <IDK@DESKTOP-4E67A7C>
2021-12-10 15:40:22 +0300

    Добавьте файлы проекта.

---
+++ AdminPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberGardenFIrst
+{
+    public class AdminPoint
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
+        public DateTime LoginTime { get; set; }
+        public bool CheckLog(AdminPoint AP)
+        {
+            AP
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberGardenFIrst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Авторизация в панель
+            AdminPoint AP = new AdminPoint();
+            Console.WriteLine("WhoOoOo are you? : ");
+            AP.Name = Console.ReadLine();
+            Console.WriteLine("Tell me our password, little boy : ");
+            AP.Password = Console.ReadLine();
+            if (AP.CheckLog(AP) == true)
+            {
+                Console.WriteLine($"Welcome, {AP.Role}: {AP.Name}. Time of Log: {AP.LoginTime}");
+            }
+            //Асинхронный запуск процесса
+
+            //Прогрузка датасета
+
+            //Генерация ключей
+
+            //
+        }
+
+
+    }
+}
